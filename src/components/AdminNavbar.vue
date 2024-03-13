7b5f0d74ee5a167a80c9267bf205593c02452e60
Ikemdinachi Uzochukwu <uzochukwuikemdinachi@gmail.com>
2024-03-13 16:50:57 +0100

    fix: re-writing functions the navbar performs; separated navbar into different components

--- src/components/AdminNavbar.vue
+++ src/components/AdminNavbar.vue
@@ -41,7 +41,7 @@ export default {
                             aria-current="page">Books</router-link>
                     </li>
                     <li>
-                        <router-link to="/"
+                        <router-link to="/customers"
                             class="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Users</router-link>
                     </li>
                     <li>
