0b343eedf227ac928907e5a83fd0faa98e3bbe89
Ikemdinachi Uzochukwu <uzochukwuikemdinachi@gmail.com>
2024-03-18 12:52:12 +0100

    commit

--- src/views/ViewBooks.vue
+++ src/views/ViewBooks.vue
@@ -1,12 +1,16 @@
 <template>
     <AdminNavbar />
     <router-view></router-view>
-    <h2>Books</h2>
-
-    <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+    <h2 class="p-5 text-lg font-semibold text-center text-gray-900 bg-white">
+        BOOKS
+    </h2>
+    <div class="relative w-screen overflow-x-auto shadow-md sm:rounded-lg">
         <table class="w-full text-sm text-left">
-            <thead class="text-xs text-gray-700 uppercase bg-gray-50">
+            <thead class="text-xs text-gray-800 uppercase bg-gray-200">
                 <tr>
+                    <th scope="col" class="px-6 py-3">
+                        ID
+                    </th>
                     <th scope="col" class="px-6 py-3">
                         Title
                     </th>
@@ -25,10 +29,13 @@
                 </tr>
             </thead>
             <tbody>
-                <tr v-for="book in books" :key="book.id" class="">
+                <tr v-for="book in books" :key="book.id" class="odd:bg-white even:bg-gray-200">
                     <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
-                        {{ book.title }}
+                        {{ book.id }}
                     </th>
+                    <td scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
+                        {{ book.title }}
+                    </td>
                     <td class="px-6 py-4">
                         {{ book.description }}
                     </td>
@@ -74,6 +81,7 @@ export default {
                 });
                 this.books = (response.data);
                 console.log(this.books);
+                console.log(token);
             } catch (error) {
                 console.error('Error fetching books:', error);
             }
