53cbb83b97472b11dda2ec92210639c50adc3a04
Ikemdinachi Uzochukwu <uzochukwuikemdinachi@gmail.com>
2024-03-20 09:06:01 +0100

    fix: set expiration time for token

--- src/router/index.js
+++ src/router/index.js
@@ -85,7 +85,7 @@ const router = createRouter({
 
 router.beforeEach((to, from, next) => {
   const token = localStorage.getItem('access_token')
-  console.log('Token:', token);
+  /* console.log('Token:', token); */
 
   if (to.matched.some(record => record.meta.requiresAuth)) {
     if (!token) {
