511cc2de92a815b8d70e5ce71ddaf2b61813f647
Cavsteeek <uzochukwuikemdinachi@gmail.com>
2024-04-08 11:18:03 +0100

    fix: made input field to capitalize first letter of each word

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -16,8 +16,9 @@
                             d="M10 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM3.465 14.493a1.23 1.23 0 0 0 .41 1.412A9.957 9.957 0 0 0 10 18c2.31 0 4.438-.784 6.131-2.1.43-.333.604-.903.408-1.41a7.002 7.002 0 0 0-13.074.003Z" />
                     </svg>
                     <!-- First name -->
-                    <input id="input" class="mt-6 font-sans px-2 h-7 text-left focus:ring-white rounded-md border-hidden"
-                        type="text" v-model="firstName" placeholder="Firstname" required capitalize>
+                    <input id="input"
+                        class="mt-6 font-sans px-2 h-7 text-left focus:ring-white rounded-md border-hidden capitalize"
+                        type="text" v-model="firstName" placeholder="Firstname" required>
 
                     &NonBreakingSpace;
                     &NonBreakingSpace;
@@ -28,8 +29,9 @@
                             d="M10 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM3.465 14.493a1.23 1.23 0 0 0 .41 1.412A9.957 9.957 0 0 0 10 18c2.31 0 4.438-.784 6.131-2.1.43-.333.604-.903.408-1.41a7.002 7.002 0 0 0-13.074.003Z" />
                     </svg>
                     <!-- Last name -->
-                    <input id="input" class="mt-6 font-sans px-2 h-7 text-left focus:ring-white rounded-md border-hidden"
-                        type="text" placeholder="Lastname" v-model="lastName" required capitalize>
+                    <input id="input"
+                        class="mt-6 font-sans px-2 h-7 text-left focus:ring-white rounded-md border-hidden capitalize"
+                        type="text" placeholder="Lastname" v-model="lastName" required>
 
                 </div>
                 <br>
