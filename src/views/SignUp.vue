19239c8d208c4a6634a01831d7cdda9fcd850fd3
Cavsteeek <uzochukwuikemdinachi@gmail.com>
2024-04-08 11:12:15 +0100

    fix: made input field to capitalize first letter of each word

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -17,7 +17,7 @@
                     </svg>
                     <!-- First name -->
                     <input id="input" class="mt-6 font-sans px-2 h-7 text-left focus:ring-white rounded-md border-hidden"
-                        type="text" v-model="firstName" placeholder="Firstname" required>
+                        type="text" v-model="firstName" placeholder="Firstname" required capitalize>
 
                     &NonBreakingSpace;
                     &NonBreakingSpace;
@@ -29,7 +29,7 @@
                     </svg>
                     <!-- Last name -->
                     <input id="input" class="mt-6 font-sans px-2 h-7 text-left focus:ring-white rounded-md border-hidden"
-                        type="text" placeholder="Lastname" v-model="lastName" required>
+                        type="text" placeholder="Lastname" v-model="lastName" required capitalize>
 
                 </div>
                 <br>
