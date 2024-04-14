7eda7df38b3f523cad9b92acdeb6c99d60923cd9
Ikemdinachi Uzochukwu <uzochukwuikemdinachi@gmail.com>
2024-04-15 00:30:06 +0100

    fix:making changes to the design in books

--- src/views/Books.vue
+++ src/views/Books.vue
@@ -18,10 +18,10 @@
                 class="max-w-sm bg-gray-800 border border-gray-200 rounded-lg shadow flex flex-col">
                 <img class="p-8 rounded-t-lg bg-cover" :src="book.imageUrl" alt="product image" />
                 <div class="px-5 pb-5 flex flex-col flex-grow">
-                    <p class="text-xl font-semibold tracking-tight text-white">{{ book.title }}</p>
-                    <p class="text-md font-semibold tracking-tight text-white">{{ book.description }}
+                    <p class="text-xl font-semibold tracking-wider text-white">{{ book.title }}</p><br>
+                    <p class="text-sm font-semibold tracking-wider text-white">Genre: {{ book.description }}
                     </p>
-                    <p class="text-sm font-semibold tracking-tight text-white">Author: {{ book.author }}</p>
+                    <p class="text-sm font-semibold tracking-wider text-white">Author: {{ book.author }}</p>
                     <div class="flex items-center mt-2.5 mb-2">
                         <div class="flex items-center space-x-1 rtl:space-x-reverse">
                             <svg v-for="star in 5" :key="star" class="w-4 h-4 text-yellow-300" aria-hidden="true"
@@ -30,8 +30,6 @@
                                     d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
                             </svg>
                         </div>
-                        <span
-                            class="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ms-3">5.0</span>
                     </div>
                     <div class="flex items-center justify-between mt-auto">
                         <span class="text-2xl font-bold text-white">₦{{ book.price }}</span>
