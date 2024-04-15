c07f0c9d568463bc56cf77e9dd643c687e3a1776
Ikemdinachi Uzochukwu <uzochukwuikemdinachi@gmail.com>
2024-04-15 17:44:24 +0100

    fix:making changes to the design in books

--- src/views/Books.vue
+++ src/views/Books.vue
@@ -12,29 +12,20 @@
             <p class="font-serif text-lg">Welcome {{ username }}!</p>
         </div><br>
 
-        <div class="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-5">
+        <div class="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-4 gap-5 text-center text-black">
 
-            <div v-for="(book) in  books " :key="book.id"
-                class="max-w-sm bg-gray-800 border border-gray-200 rounded-lg shadow flex flex-col">
-                <img class="p-8 rounded-t-lg bg-cover" :src="book.imageUrl" alt="product image" />
-                <div class="px-5 pb-5 flex flex-col flex-grow">
-                    <p class="text-xl font-semibold tracking-wider text-white">{{ book.title }}</p><br>
-                    <p class="text-sm font-semibold tracking-wider text-white">Genre: {{ book.description }}
+            <div v-for="(book) in  books " :key="book.id" class="max-w-xs rounded-lg flex flex-col">
+                <img class="object-scale-down mt-2 w-full h-48 rounded-t-lg bg-cover" :src="book.imageUrl"
+                    alt="product image" />
+                <div class="px-4 pb-2 flex flex-col flex-grow">
+                    <p class="text-lg font-semibold tracking-wider mt-2 mb-2">{{ book.title }}</p>
+                    <p class="text-sm font-semibold tracking-wider">Genre: {{ book.description }}
                     </p>
-                    <p class="text-sm font-semibold tracking-wider text-white">Author: {{ book.author }}</p>
-                    <div class="flex items-center mt-2.5 mb-2">
-                        <div class="flex items-center space-x-1 rtl:space-x-reverse">
-                            <svg v-for="star in 5" :key="star" class="w-4 h-4 text-yellow-300" aria-hidden="true"
-                                xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
-                                <path
-                                    d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                            </svg>
-                        </div>
-                    </div>
-                    <div class="flex items-center justify-between mt-auto">
-                        <span class="text-2xl font-bold text-white">₦{{ book.price }}</span>
+                    <p class="text-sm font-semibold tracking-wider text-black">Author: {{ book.author }}</p>
+                    <div class="flex flex-col items-center mt-2.5">
+                        <span class="text-xl font-bold mb-2">₦{{ book.price }}</span>
                         <a href="#"
-                            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Add
+                            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-1.5 text-center">Add
                             to cart</a>
                     </div>
                 </div>
