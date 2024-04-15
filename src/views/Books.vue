92bbbb6d32c4ff537d709858240dcb4bc1f1192f
Ikemdinachi Uzochukwu <uzochukwuikemdinachi@gmail.com>
2024-04-15 17:21:56 +0100

    fix:making changes to the design in books

--- src/views/Books.vue
+++ src/views/Books.vue
@@ -12,7 +12,7 @@
             <p class="font-serif text-lg">Welcome {{ username }}!</p>
         </div><br>
 
-        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5">
+        <div class="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-5">
 
             <div v-for="(book) in  books " :key="book.id"
                 class="max-w-sm bg-gray-800 border border-gray-200 rounded-lg shadow flex flex-col">
