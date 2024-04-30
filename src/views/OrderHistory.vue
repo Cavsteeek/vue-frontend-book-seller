b304daeed0d0e982da892398d7b121f44815c412
Ikemdinachi Uzochukwu <uzochukwuikemdinachi@gmail.com>
2024-04-30 14:33:56 +0100

    feat: making the order history page for admin

--- src/views/OrderHistory.vue
+++ src/views/OrderHistory.vue
@@ -6,62 +6,110 @@
         <p>You are not authorized to access this page.</p>
     </div>
 
-    <div class="px-5 py-5" v-if="userRole === 'ADMIN'">
-        <table class="table-fixed border-separate m-auto w-full">
-            <div class="">
-                <thead>
-                    <tr class="text-left px-2 py-2">
-                        <th>Id</th>
-                        <th>User ID</th>
-                        <th>Book ID</th>
-                        <th>Book</th>
-                        <th>Price</th>
+    <div v-if="userRole === 'ADMIN'">
+
+        <h2 class="p-5 text-xl ml-40 mb-2 font-raleway font-semibold text-center text-gray-900 bg-white">
+            ORDER
+        </h2>
+
+        <div class="relative w-screen overflow-x-auto shadow-md rounded-xl sm:rounded-lg" style="max-height: 400px;">
+            <table class="w-full text-sm text-left">
+                <thead class="text-xs text-gray-800 uppercase bg-gray-200">
+                    <tr>
+                        <th scope="col" class="px-6 py-3">
+                            ID
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            Purchase Time
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            Quantity
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            Price
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            Book ID
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            User ID
+                        </th>
                     </tr>
                 </thead>
-            </div>
-            <div class="bg-slate-300">
-                <tbody>
-                    <tr class="text-left font-semibold">
-                        <td class="">1</td>
-                        <td class=""></td>
-                        <td class=""></td>
-                        <td class=""></td>
-                        <td class=""></td>
-                    </tr>
-                    <tr class="text-left font-semibold">
-                        <td class="">2</td>
-                        <td class=""></td>
-                        <td class=""></td>
-                        <td class=""></td>
-                        <td class=""></td>
-                    </tr>
-                    <tr class="text-left font-semibold">
-                        <td class="">3</td>
-                        <td class=""></td>
-                        <td class=""></td>
-                        <td class=""></td>
-                        <td class=""></td>
+                <tbody class="overflow-y-auto">
+                    <tr v-for="(order, index) in orderHistory" :key="order.id" class="odd:bg-white even:bg-gray-200">
+                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
+                            {{ index + 1 }}
+                        </th>
+                        <td class="px-6 py-4">
+                            {{ order.purchaseTime }}
+                        </td>
+                        <td class="px-6 py-4 ">
+                            {{ order.price }}
+                        </td>
+                        <td class="px-6 py-4 ">
+                            {{ order.quantity }}
+                        </td>
+
+                        <td class="px-6 py-4">
+                            {{ order.bookId }}
+                        </td>
+                        <td class="px-6 py-4">
+                            {{ order.userId }}
+                        </td>
                     </tr>
                 </tbody>
-            </div>
-        </table>
-    </div>
-    <div>
-
+            </table>
+        </div>
     </div>
 </template>
 
+
 <script>
 import AdminNavbar from '@/components/AdminNavbar.vue';
+import axios from 'axios'
+import 'flowbite'
+
 export default {
     name: "OrderHistory",
     components: { AdminNavbar },
     data() {
         return {
+            orderHistory: [],
             userRole: localStorage.getItem('user_role') || '',
-        }
-    }
+        };
+    },
 
+    methods: {
+        async getOrderHistory() {
+            const token = localStorage.getItem("access_token");
+            const apiUrl = 'http://localhost:8080/api/v1/purchases/all-purchases';
+
+            await axios.get(apiUrl, {
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                }
+            })
+                .then(response => {
+                    if (response.status >= 200 && response.status < 300) {
+                        this.orderHistory = response.data;
+                        console.log(this.orderHistory);
+                        console.log("Token: ", token);
+                    } else {
+                        alert(`you must be signed in to view this...confirm you are signed in`)
+                        console.log(response.data)
+                    }
+                })
+                .catch(error => {
+                    console.error('Error Fecthing Order History:', error);
+                });
+        },
+
+    },
+
+    mounted() {
+        this.getOrderHistory();
+    }
 }
 </script>
 
