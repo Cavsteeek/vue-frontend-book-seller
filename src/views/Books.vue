f29eabbd36e03dc5e0ca0fd5417894f4863e12b4
Ikemdinachi Uzochukwu <uzochukwuikemdinachi@gmail.com>
2024-04-30 09:36:09 +0100

    fix: making changes to the design of the books.vue class

--- src/views/Books.vue
+++ src/views/Books.vue
@@ -1,4 +1,4 @@
-<template>
+<!-- <template>
     <NavBar v-if="userRole === 'USER'" />
     <router-view v-if="userRole === 'USER'"></router-view>
 
@@ -28,6 +28,46 @@
                         <span class="text-xl font-bold mb-2">₦{{ book.price }}</span>
                         <a href="#"
                             class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-md text-sm px-4 py-1.5 text-center">Add
+                            to cart</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template> -->
+
+
+<template>
+    <NavBar v-if="userRole === 'USER'" />
+    <router-view v-if="userRole === 'USER'"></router-view>
+
+    <div v-if="userRole !== 'USER'" class="text-red-500 text-center text-3xl mt-20">
+        <p>You are not authorized to access this page.</p>
+    </div>
+
+    <div class="px-4 sm:px-5 py-5" v-if="userRole === 'USER'">
+        <div>
+            <p class="text-base sm:text-lg font-serif">Welcome {{ username }}!</p>
+        </div><br>
+
+        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 md:gap-5 text-center text-black">
+            <div v-for="(book, index) in books" :key="index"
+                class="max-w-xs mx-auto rounded-lg overflow-hidden flex flex-col items-center">
+                <img class="mt-2 w-full h-48 rounded-t-lg object-cover" :src="book.imageUrl" alt="product image" />
+                <div class="p-4 flex flex-col items-center flex-grow">
+                    <p class="text-sm sm:text-lg font-semibold tracking-wider mt-2 mb-2 whitespace-nowrap url-field"
+                        :title="book.title">
+                        {{ book.title }}
+                    </p>
+                    <p class="text-xs sm:text-sm font-semibold tracking-wider" :title="book.description">Genre: {{
+                        book.description }}
+                    </p>
+                    <p class="text-xs sm:text-sm font-semibold tracking-wider" :title="book.author">Author: {{ book.author
+                    }}</p>
+                    <div class="flex flex-col items-center mt-2.5">
+                        <span class="text-lg sm:text-xl font-bold mb-2">₦{{ book.price }}</span>
+                        <a href="#"
+                            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-md text-xs sm:text-sm px-3 py-1.5 text-center">Add
                             to cart</a>
                     </div>
                 </div>
